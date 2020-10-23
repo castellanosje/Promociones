fe3616dad3eafa9e9b8dace5fca3dd116298ccaa
je34429 <je34429@gmail.com>
2020-10-23 10:18:04 -0500

    Transformamos el código de Java a Kotlin, creamos las actividades de bienvenida y registro, importamos el plugin de kotlin para navegar de una vista a la otra usando botones

---
+++ app/src/main/java/proyecto/umb/promociones/MainActivity.kt
@@ -0,0 +1,18 @@
+package proyecto.umb.promociones
+
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import proyecto.umb.promociones.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ingresar.setOnClickListener {
+            startActivity(Intent(this,Registro::class.java))
+        }
+    }
+}
